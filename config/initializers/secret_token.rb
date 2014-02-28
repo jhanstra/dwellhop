0789560e67dad7843c47dddef55ce6cb4895140d
Jared Hanstra <jhanstra@gmail.com>
2014-02-27 21:46:00 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dwellhop::Application.config.secret_key_base = '14c74bcc3e61ecef7e17c994023b4a0dda81d8c597217df3efb6db5c5ecb7e500025d361fbac0939d6fcdf0d41dc94ddc50c84d33708a83d9774d7be22473afd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dwellhop::Application.config.session_store :cookie_store, key: '_dwellhop_session'
