c6cbfd58256ac8c46a2bc4f5b2f2dc0c31999a85
Jared Hanstra <jhanstra@gmail.com>
2014-03-01 14:55:24 -0600

    static pages

--- Gemfile
+++ Gemfile
@@ -2,7 +2,6 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '4.0.1'
  # Rails defaults
-gem 'sqlite3'
 gem 'sass-rails', '~> 4.0.0' 
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0' 
@@ -18,8 +17,25 @@ gem 'google_drive'
 gem 'high_voltage'
 gem 'simple_form'
 gem 'zurb-foundation'
+
 group :development do
 	gem 'better_errors'
 	gem 'quiet_assets' 
 	gem 'rails_layout'
+	gem 'sqlite3', '1.3.8'
+	gem 'rspec-rails', '2.13.1'
+end
+
+group :test do
+  gem 'selenium-webdriver', '2.35.1'
+  gem 'capybara', '2.1.0'
+end
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
+group :production do
+  gem 'pg', '0.15.1'
+  gem 'rails_12factor', '0.0.2'
 end
---
+++ spec/requests/statics_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static Pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Home'" do
+      visit '/static/home'
+      expect(page).to have_content('Home')
+    end
+    it "should have the right title" do
+	  visit '/static/home'
+	  expect(page).to have_title("Dwellhop | Home")
+	end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About'" do
+      visit '/static/about'
+      expect(page).to have_content('About')
+    end
+    it "should have the right title" do
+	  visit '/static/about'
+	  expect(page).to have_title("Dwellhop | About")
+	end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static/help'
+      expect(page).to have_content('Help')
+    end
+    it "should have the right title" do
+	  visit '/static/help'
+	  expect(page).to have_title("Dwellhop | Help")
+	end
+  end
+end
