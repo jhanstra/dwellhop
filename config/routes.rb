c381fa643f5a015552b6faee46299198cf5dacbe
Jared Hanstra <jhanstra@gmail.com>
2014-03-12 15:58:18 -0500

    add sign-in/sign-out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Dwellhop::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root  'static#home'
   match '/signup',  to: 'users#new',      via: 'get'
+  match '/signin',  to: 'sessions#new',   via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   match '/help',    to: 'static#help',    via: 'get'
   match '/about',   to: 'static#about',   via: 'get'
 
