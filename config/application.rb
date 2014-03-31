03ad5eb7dd311b6dbde45c9728d260596647f009
Jared Hanstra <jhanstra@gmail.com>
2014-03-31 18:58:21 -0500

    Try to fix paperclip issues

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Dwellhop
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-
+    config.active_record.whitelist_attributes = true
   end
 end
