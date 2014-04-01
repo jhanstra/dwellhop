064802c44aac5b9ec188eb0341235db821a117e4
Jared Hanstra <jhanstra@gmail.com>
2014-03-31 20:58:44 -0500

    fix sign up issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)    
 		if @user.save
 			flash[:success] = "Welcome to Dwellhop!"
+      sign_in @user
       redirect_to @user
     else
       render 'new'
