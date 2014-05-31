25f8f566aa73afffa6d62b3bb5d3bf3566dd79ef
Jared Hanstra <jhanstra@gmail.com>
2014-05-30 20:28:40 -0500

    precompile assets again

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Dwellhop
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.enabled = true
     config.active_record.whitelist_attributes = true
   end
 end
